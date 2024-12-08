87a4e1ab6bb8ca42ce9f076f311b6fbb1d1a295d
Laplaza1 <skysavage55@gmail.com>
2024-12-08 07:40:11 -0600

    added a .gitignore page

--- __init__.py
+++ __init__.py
@@ -6,3 +6,6 @@ def install_requirements():
         subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while installing packages: {e}")
+    except ModuleNotFoundError:
+        subprocess.check_call([sys.executable, "-m", "pip3.12", "install", "-r", "requirements.txt"])
+        
